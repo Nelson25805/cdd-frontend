c495745499a532f1a9aeb4cb9edacde6229b4831
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2024-08-21 21:57:11 -0400

    Added privateroute logic for certain pages to only be accessible if logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './Context/UserContext';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
@@ -12,6 +13,7 @@ import Search from './Pages/Search';
 import AccountSettings from './Pages/AccountSettings';
 import EditGameDetails from './Pages/EditGameDetails';
 import ReportsMenu from './Pages/ReportsMenu';
+import PrivateRoute from './Context/PrivateRoute';
 
 const App = () => {
   return (
@@ -19,16 +21,20 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/GameDetails" element={<GameDetails />} />
-          <Route path="/AddGameToDatabase" element={<AddGameToDatabase />} />
-          <Route path="/MyCollection" element={<MyCollection />} />
-          <Route path="/MyWishlist" element={<MyWishlist />} />
-          <Route path="/Search" element={<Search />} />
-          <Route path="/AccountSettings" element={<AccountSettings />} />
-          <Route path="/EditGameDetails" element={<EditGameDetails />} />
-          <Route path="/ReportsMenu" element={<ReportsMenu />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          
+          {/* Protected routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/GameDetails" element={<GameDetails />} />
+            <Route path="/AddGameToDatabase" element={<AddGameToDatabase />} />
+            <Route path="/MyCollection" element={<MyCollection />} />
+            <Route path="/MyWishlist" element={<MyWishlist />} />
+            <Route path="/Search" element={<Search />} />
+            <Route path="/AccountSettings" element={<AccountSettings />} />
+            <Route path="/EditGameDetails" element={<EditGameDetails />} />
+            <Route path="/ReportsMenu" element={<ReportsMenu />} />
+          </Route>
         </Routes>
       </Router>
     </UserProvider>
