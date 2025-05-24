3af5d019a9eafd0204f8100c47a8d691b5746790
Nelson <Nelson25805@hotmail.com>
2025-05-23 22:15:23 -0400

    removed all warnings from starrating

--- src/Context/StarRating.jsx
+++ src/Context/StarRating.jsx
@@ -1,29 +1,39 @@
+import PropTypes from 'prop-types';
+
 // StarRating component for displaying and selecting ratings
 const StarRating = ({ rating, onChange, starSize = 24 }) => {
-    const stars = [1, 2, 3, 4, 5];
+  const stars = [1, 2, 3, 4, 5];
 
-    return (
-        <div>
-            {stars.map((star) => {
-                const isHalfStar = rating >= star - 0.5 && rating < star;
-                const isFullStar = rating >= star;
+  return (
+    <div>
+      {stars.map((star) => {
+        const isHalfStar = rating >= star - 0.5 && rating < star;
+        const isFullStar = rating >= star;
 
-                return (
-                    <span
-                        key={star}
-                        style={{
-                            cursor: 'pointer',
-                            color: isFullStar ? 'gold' : 'gray',
-                            fontSize: `${starSize}px`,
-                        }}
-                        onClick={() => onChange(isHalfStar ? star - 0.5 : star)}
-                    >
-                        {isHalfStar ? '☆' : '★'}
-                    </span>
-                );
-            })}
-        </div>
-    );
+        return (
+          <span
+            key={star}
+            style={{
+              cursor: 'pointer',
+              color: isFullStar ? 'gold' : 'gray',
+              fontSize: `${starSize}px`,
+            }}
+            onClick={() => onChange(isHalfStar ? star - 0.5 : star)}
+          >
+            {isHalfStar ? '☆' : '★'}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
+
+StarRating.propTypes = {
+  rating:    PropTypes.number.isRequired,
+  onChange:  PropTypes.func.isRequired,
+  starSize:  PropTypes.number,
+};
+
+// NO defaultProps block needed now
 
 export default StarRating;
